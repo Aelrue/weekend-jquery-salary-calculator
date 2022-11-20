f6095cdb0234987d09d6ecffb6404f8b9ee4cd9b
Aelrue <101987454+Aelrue@users.noreply.github.com>
2022-11-20 12:26:37 -0600

    added for loop, empty, append to remainingBudget function; css to costOut

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log("ready!");
-  $(".submit-button").on("click", addEmployee);
+  $("#submitEmployeeButton").on("click", addEmployee);
 }
 
 function addEmployee() {
@@ -20,7 +20,30 @@ function addEmployee() {
     annualSalary: $("#salaryIn").val(),
   };
 
+  employees.push(employeeInfo);
+  $("#firstIn").val("");
+  $("#lastIn").val("");
+  $("#identifierIn").val("");
+  $("#titleIn").val("");
+  $("#salaryIn").val("");
   render();
+  calculateRemainingBudget();
+}
+
+function calculateRemainingBudget() {
+  console.log("in calculateRemainingBudget");
+  let sum = 0;
+  for (let i = 0; i < employees.length; i++) {
+    sum += Number(employees[i].annualSalary);
+  }
+  console.log("Monthly Cost:", sum);
+
+  let el = $("#costOut");
+  el.empty();
+  el.append(sum);
+  if (sum > 20000) {
+    $("#costOut").css("background-color", "red");
+  }
 }
 
 function render() {
