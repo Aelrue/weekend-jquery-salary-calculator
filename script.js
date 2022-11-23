5d59c5fa912f47900350b5eccc69e6c6b8c92b86
Aelrue <101987454+Aelrue@users.noreply.github.com>
2022-11-22 21:20:00 -0600

    need to fix delete function and add new function to empty DOM

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// empty array for employee information
+// start with empty array for employee information
 let employees = [];
 
 console.log("script sourced");
 
 $(document).ready(onReady);
 
+// onReady function will run on page load and wait for click events
 function onReady() {
   console.log("ready!");
   $("#submitEmployeeButton").on("click", addEmployee);
-  $(".delete").on("click", deleteEmployee);
+  $("#employee-table").on("click", ".delete-button", deleteEmployee);
 }
 
 function addEmployee() {
   console.log("Clicked!");
 
+  // create an object from form inputs
   let employeeInfo = {
     firstName: $("#firstIn").val(),
     lastName: $("#lastIn").val(),
@@ -22,14 +24,20 @@ function addEmployee() {
     annualSalary: $("#salaryIn").val(),
   };
 
+  // push that info into the employees array for storage
   employees.push(employeeInfo);
+
+  // clear input fields for further use
   $("#firstIn").val("");
   $("#lastIn").val("");
   $("#identifierIn").val("");
   $("#titleIn").val("");
   $("#salaryIn").val("");
 
+  //Prior to loop, use .empty to remove repeating content in future loops
   $("#employee-table").empty();
+
+  // loop through array and add info to DOM
   for (employee of employees) {
     $("#employee-table").append(`
     <tr>
@@ -38,11 +46,17 @@ function addEmployee() {
       <td>${employee.identifier}</td>
       <td>${employee.title}</td>
       <td>${employee.annualSalary}</td>
-      <th><button id="delete-button">Delete</button></th>
+      <td><button class="delete-button">Delete</button></th>
     </tr>`);
   }
+  // on line 40, the loop and append - pull that out into its own function
+  // then, call that function on line 40 (instead of all the old code)
+  // add line 38 to the new function to empty the dom
+  // and then, in our delete function, after we’ve removed the employee from the employees array, we’ll call that NEW loop/append function
 
   console.log(employees);
+
+  // call calculate function here
   calculateRemainingBudget();
 }
 
@@ -64,5 +78,6 @@ function calculateRemainingBudget() {
 
 function deleteEmployee() {
   console.log("Deleted!");
-  $(this).parent().parent().empty();
+  $(this).parent().parent().remove();
+  // remove $(this) from the employee array
 }
